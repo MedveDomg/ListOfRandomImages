148bc362a6f311b3b5893e78bc90c00705ce9cd6
Vadim Zhdanov <zhdanov.vadim.alexandrovich@gmail.com>
2016-12-01 01:24:51 +0200

    add CardView to image_row_item

--- src/main/java/omg/medvedomg/listofrandomimages/MainActivity.java
+++ src/main/java/omg/medvedomg/listofrandomimages/MainActivity.java
@@ -2,12 +2,17 @@ package omg.medvedomg.listofrandomimages;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
 import omg.medvedomg.listofrandomimages.model.Image;
 import omg.medvedomg.listofrandomimages.model.ImageResponse;
+import omg.medvedomg.listofrandomimages.recyclerviewadapter.ImageAdapter;
 import omg.medvedomg.listofrandomimages.rest.ApiClient;
 import omg.medvedomg.listofrandomimages.rest.ApiInterface;
 import retrofit2.Call;
@@ -25,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
         Call<ImageResponse> call = apiService.getImageList();
         call.enqueue(new Callback<ImageResponse>() {
             @Override
@@ -37,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.d(TAG, "images downloaded" + images.get(3).getId());
 
-//                recyclerView.setAdapter(new JokeAdapter(R.layout.joke_list_item, jokes, MainActivity.this));
+                recyclerView.setAdapter(new ImageAdapter(getApplicationContext(),images));
             }
 
             @Override
--- src/main/java/omg/medvedomg/listofrandomimages/recyclerviewadapter/ImageAdapter.java
+++ src/main/java/omg/medvedomg/listofrandomimages/recyclerviewadapter/ImageAdapter.java
@@ -2,11 +2,18 @@ package omg.medvedomg.listofrandomimages.recyclerviewadapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.zip.Inflater;
 
+import omg.medvedomg.listofrandomimages.R;
 import omg.medvedomg.listofrandomimages.model.Image;
 
 /**
@@ -25,22 +32,33 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
     public static class ImageViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView imageView;
+        TextView textView;
+
         public ImageViewHolder(View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            textView = (TextView) itemView.findViewById(R.id.idImage);
         }
     }
     @Override
     public ImageAdapter.ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_row_item, parent,false);
+        return new ImageViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ImageAdapter.ImageViewHolder holder, int position) {
+        Picasso.with(context)
+                .load(images.get(position).getUrl().toString())
+                .fit()
+                .into(holder.imageView);
+        holder.textView.setText(images.get(position).getId()+"4");
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.size();
     }
 }
